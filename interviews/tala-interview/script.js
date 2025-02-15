306cf4bf6cce14d4b887f3bbada98d601744575c
Richard M <160991580+richarrd92@users.noreply.github.com>
2025-02-14 20:22:57 -0500

    ..

--- interviews/tala-interview/script.js
+++
@@ -1,8 +0,0 @@
-// Get the button element(s) by class name
-const btn = document.getElementById("add-to-cart-button");
-
-// Add a click event listener to each button
-btn.addEventListener("click", () => {
-  // Get the parent div of the button
-  btn.style.backgroundColor = "Red";
-});
