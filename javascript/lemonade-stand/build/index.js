5e11ba0a442499509b510e73f3d2738453898248
Richard M <160991580+richarrd92@users.noreply.github.com>
2025-09-29 17:32:37 -0400

    initialize lemonade stand app

---
+++ javascript/lemonade-stand/build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log("hello world");
+var func = function func() {
+  console.log("hello world from func");
+};
+var func2 = function func2() {
+  return "hello world from func2";
+};
---
+++ javascript/lemonade-stand/src/index.js
@@ -0,0 +1,9 @@
+console.log("hello world");
+
+const func = () => {
+  console.log("hello world from func");
+};
+
+const func2 = () => {
+  return "hello world from func2";
+};
