9a5ba8c021bbe1aa11e2913501b24bde9fbe55e7
Richard M <160991580+richarrd92@users.noreply.github.com>
2025-09-30 12:30:17 -0400

    call-back functions

--- javascript/lemonade-stand/src/index.js
+++ javascript/lemonade-stand/src/index.js
@@ -1,9 +1,18 @@
 console.log("hello world");
 
-const func = () => {
-  console.log("hello world from func");
+// lambda functions
+const func = (timelapse=0) => {
+  console.log("hello world from func, timelapse: ", timelapse);
 };
 
-const func2 = () => {
-  return "hello world from func2";
+const func2 = (timelapse=0) => {
+  return "hello world from func2 timelapse: " + timelapse;
 };
+
+const func3 = (timelapse=0) => "hello world from func3 timelapse: " + timelapse;
+
+// call-back functions
+setTimeout(() => func(1000), 1000);
+console.log(func2());
+setTimeout(() => console.log(func3(5000)), 5000);
+
