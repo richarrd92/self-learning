3009b81a7ba55bda6bbec9d8e5f4d469b4511b3e
Richard M <160991580+richarrd92@users.noreply.github.com>
2026-08-14 14:56:42 -0400

    migrate code to smaller components

--- angular/manager-board/src/app/dashboard/traffic/traffic.component.ts
+++ angular/manager-board/src/app/dashboard/traffic/traffic.component.ts
@@ -5,8 +5,39 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './traffic.component.html',
-  styleUrl: './traffic.component.css'
+  styleUrl: './traffic.component.css',
 })
 export class TrafficComponent {
+  dummyTrafficData = [
+    {
+      id: 'd1',
+      value: 433,
+    },
+    {
+      id: 'd2',
+      value: 260,
+    },
+    {
+      id: 'd3',
+      value: 290,
+    },
+    {
+      id: 'd4',
+      value: 410,
+    },
+    {
+      id: 'd5',
+      value: 397,
+    },
+    {
+      id: 'd6',
+      value: 488,
+    },
+    {
+      id: 'd47',
+      value: 589,
+    },
+  ];
 
+  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
 }
