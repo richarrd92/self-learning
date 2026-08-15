bc7c841608b6ddf3be8e1a63f524b4d31a9446ad
Richard M <160991580+richarrd92@users.noreply.github.com>
2026-08-15 15:15:06 -0400

    migrate css and practice angular lifecycles

--- angular/manager-board/src/app/dashboard/server-status/server-status.component.ts
+++ angular/manager-board/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -6,7 +6,27 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
+  // encapsulation: ViewEncapsulation.None
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit, AfterViewInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  private interval?: ReturnType<typeof setInterval>;
+
+  ngOnInit() {
+    console.log('OnInit ServerStatusComponent');
+    this.interval = setInterval(() => {
+      const rnd = Math.random();
+      if(rnd > 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000)
+  }
+
+  ngAfterViewInit() {
+    console.log('ServerStatusComponent view initialized');
+  }
 }
