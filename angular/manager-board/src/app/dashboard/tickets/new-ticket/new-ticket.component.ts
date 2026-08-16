81d1ec8082e1315fc5e6457193748b620f1f3dc2
Richard M <160991580+richarrd92@users.noreply.github.com>
2026-08-16 08:27:37 -0400

    added ticket component to render multiple new tickets

--- angular/manager-board/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ angular/manager-board/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,7 +1,8 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { ButtonComponent } from "../../../shared/button/button.component";
+import { Component, ElementRef, input, output, ViewChild } from '@angular/core';
+import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
+import { Ticket } from '../ticket.model';
 
 @Component({
   selector: 'app-new-ticket',
@@ -12,16 +13,25 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTicketComponent {
   @ViewChild('newTicketForm') newTicketForm?: ElementRef<HTMLFormElement>;
-  submittedTicket?: { title: string, request: string };
+  submittedTicket?: { title: string; request: string };
+
+  addTicket = output<Ticket>();
 
   onSubmit(title: HTMLInputElement, request: HTMLTextAreaElement) {
-    this.submittedTicket = {
-      title: title.value,
-      request: request.value
-    }
+    // this.submittedTicket = {
+    //   title: title.value,
+    //   request: request.value
+    // }
 
-    console.log(this.submittedTicket.title);
-    console.log(this.submittedTicket.request);
+    this.addTicket.emit({
+      id: Math.random().toString(),
+      title: title.value,
+      request: request.value,
+      status: 'open',
+    });
+
+    // console.log(this.submittedTicket.title);
+    // console.log(this.submittedTicket.request);
     this.newTicketForm?.nativeElement.reset();
   }
 }
--- angular/manager-board/src/app/dashboard/tickets/tickets.component.ts
+++ angular/manager-board/src/app/dashboard/tickets/tickets.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { NewTicketComponent } from "./new-ticket/new-ticket.component";
+import { Ticket } from './ticket.model';
+import { TicketComponent } from "./ticket/ticket.component";
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent, TicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css'
 })
-export class TicketsComponent {}
+export class TicketsComponent {
+  tickets: Ticket[] = []
+
+  onNewTicket(ticket: Ticket) {
+    this.tickets.push(ticket)
+  }
+}
